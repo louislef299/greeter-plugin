69063ed498db2040f1d689b80fe0c1d63fc97ba5
Louis LeFebvre <lefebl4@medtronic.com>
2025-12-02 16:19:18 -0600

    feat: Implement main function

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/louislef299/greeter-plugin/shared"
+)
+
 func main() {
-	print("hello, greeter!")
+	// We're a host. Start by launching the plugin process.
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins:         shared.PluginMap,
+		Cmd:             exec.Command("sh", "-c", os.Getenv("KV_PLUGIN")),
+		AllowedProtocols: []plugin.Protocol{
+			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
+	})
+	defer client.Kill()
+
+	// Connect via RPC
+	rpcClient, err := client.Client()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Request the plugin
+	raw, err := rpcClient.Dispense("greet")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// We should have a Greeter now! This feels like a normal interface
+	// implementation but is in fact over an RPC connection.
+	g := raw.(shared.Greet)
+	os.Args = os.Args[1:]
+	fmt.Println(g.Greet(os.Args[1]))
 }
--- shared/interface.go
+++ shared/interface.go
@@ -9,6 +9,14 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Handshake is a common handshake that is shared by plugin and host.
+var Handshake = plugin.HandshakeConfig{
+	// This isn't required when using VersionedPlugins
+	ProtocolVersion:  1,
+	MagicCookieKey:   "BASIC_PLUGIN",
+	MagicCookieValue: "hello",
+}
+
 // PluginMap is the map of plugins we can dispense.
 var PluginMap = map[string]plugin.Plugin{
 	"greet": &GreetPlugin{},
